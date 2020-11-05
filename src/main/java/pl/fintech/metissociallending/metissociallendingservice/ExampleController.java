d2f96882973f3dbb72093df99147554ad9b3012c
Paweł Potuczko <pawelpo99@gmail.com>
2020-11-05 23:26:26 +0100

    Created profiles for local, sit, uat and prod with kubernetes deployment config

--- src/main/java/pl/fintech/metissociallending/metissociallendingservice/ExampleController.java
+++ src/main/java/pl/fintech/metissociallending/metissociallendingservice/ExampleController.java
@@ -1,5 +1,6 @@
 package pl.fintech.metissociallending.metissociallendingservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ExampleController {
 
+    @Value("${hello.value:value}")
+    private String version;
+
     @GetMapping
     public String example() {
-        return "Hello FinTech!";
+
+        return "Hello " + version + " FinTech!";
     }
 
 }
