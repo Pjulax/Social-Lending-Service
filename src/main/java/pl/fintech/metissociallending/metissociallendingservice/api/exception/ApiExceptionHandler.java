f2994dcf9875c523f67088e7df939c3d09b751cc
Paweł Potuczko <pawelpo99@gmail.com>
2020-11-20 15:02:35 +0100

    Provided temporary loggers and exception handler for all exceptions

--- src/main/java/pl/fintech/metissociallending/metissociallendingservice/api/exception/ApiExceptionHandler.java
+++ src/main/java/pl/fintech/metissociallending/metissociallendingservice/api/exception/ApiExceptionHandler.java
@@ -1,35 +1,51 @@
 package pl.fintech.metissociallending.metissociallendingservice.api.exception;
 
-import org.springframework.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.NoSuchElementException;
+
 import io.micrometer.core.instrument.config.validate.ValidationException;
-import org.springframework.web.context.request.WebRequest;
 import pl.fintech.metissociallending.metissociallendingservice.api.BorrowerController;
 import pl.fintech.metissociallending.metissociallendingservice.api.LenderController;
 import pl.fintech.metissociallending.metissociallendingservice.api.UserController;
 
 @ControllerAdvice(basePackageClasses = {UserController.class, LenderController.class, BorrowerController.class})
 public class ApiExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex){
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Object> handleValidationException(ValidationException e){
-        return new ResponseEntity<>(e.getValidation(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleValidationException(ValidationException ex){
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getValidation(), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(ExistingObjectException.class)
     public ResponseEntity<Object> handleExistingUserException(ExistingObjectException ex) {
-        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleExistingUserException(IllegalArgumentException ex) {
-        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
-
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeExceptions(RuntimeException ex) {
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleExceptions(Exception ex) {
+        log.error("Unexpected error!", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
